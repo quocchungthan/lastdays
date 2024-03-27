7bfbbc038acb1061c5617df65e347d382a18b2a2
Chung Than Quoc <thanc.ext@medgate.ch>
2024-03-27 08:13:58 +0700

    install konva and setup directory

---
+++ reactive-UI/src/ultilities/painting/test-konva-ng2/test-konva-ng2.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-test-konva-ng2',
+  standalone: true,
+  imports: [],
+  templateUrl: './test-konva-ng2.component.html',
+  styleUrl: './test-konva-ng2.component.scss'
+})
+export class TestKonvaNg2Component {
+
+}
