366bccfe7166a9ae94b37e37a0d68e12b818153d
Chung Than Quoc <thanc.ext@medgate.ch>
2024-03-27 09:02:12 +0700

    embed the new component to main

--- reactive-UI/src/app/app.component.ts
+++ reactive-UI/src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { drawingJSON } from '../ultilities/painting/data-to-migrate';
+import { TestKonvaNg2Component } from '../ultilities/painting/test-konva-ng2/test-konva-ng2.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TestKonvaNg2Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'reactive-ui';
+  testData = drawingJSON;
 }
--- reactive-UI/src/ultilities/painting/test-konva-ng2/test-konva-ng2.component.ts
+++ reactive-UI/src/ultilities/painting/test-konva-ng2/test-konva-ng2.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { DrawingBearer } from '../communication-objects/DrawingObject.fabric';
 
 @Component({
-  selector: 'app-test-konva-ng2',
+  selector: 'test-konva-ng2',
   standalone: true,
   imports: [],
   templateUrl: './test-konva-ng2.component.html',
   styleUrl: './test-konva-ng2.component.scss'
 })
 export class TestKonvaNg2Component {
-
+  @Input()
+  data: DrawingBearer = {
+    version: '1.1.1',
+    objects: [],
+    height: 0,
+    width: 0,
+  };
 }
