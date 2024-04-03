7ba94828f1dc7a4dea29f3f97bcc70e8d8106092
Chung Than Quoc <thanc.ext@medgate.ch>
2024-04-03 12:28:49 +0700

    polygon seems ok but the ratio of increment does not good

--- reactive-UI/src/app/pages/break-down/break-down.component.ts
+++ reactive-UI/src/app/pages/break-down/break-down.component.ts
@@ -1,14 +1,16 @@
 import { Component, Input } from '@angular/core';
 import { Problem } from './models/Problem';
+import { testData } from './test-data';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-break-down',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './break-down.component.html',
   styleUrl: './break-down.component.scss'
 })
 export class BreakDownComponent {
   @Input()
-  data: Problem[] = [];
+  data: Problem[] = testData;
 }
---
+++ reactive-UI/src/app/pages/break-down/test-data.ts
@@ -0,0 +1,14 @@
+import { Problem } from './models/Problem';
+
+export const testData: Problem[] = [
+    {
+        id: '1',
+        uncertaintyScore: 99,
+        name: 'better performance in drawings view-only'
+    },
+    {
+        id: '2',
+        uncertaintyScore: 1,
+        name: 'keep the story living'
+    }
+];
