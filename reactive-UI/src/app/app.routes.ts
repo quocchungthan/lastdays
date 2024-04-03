540e390b17cf9d7a5c7b1969a883129543272f34
Chung Than Quoc <thanc.ext@medgate.ch>
2024-04-03 11:25:49 +0700

    minus the space of sidebar

--- reactive-UI/src/app/app.routes.ts
+++ reactive-UI/src/app/app.routes.ts
@@ -1,10 +1,16 @@
 import { Routes } from '@angular/router';
 import { TestKonvaNg2Component } from '../ultilities/painting/test-konva-ng2/test-konva-ng2.component';
+import { BreakDownComponent } from './pages/break-down/break-down.component';
 
 export const routes: Routes = [
     {
         path: 'konva',
         component: TestKonvaNg2Component,
+        pathMatch: 'full'
+    },
+    {
+        path: 'expiriments/break-down',
+        component: BreakDownComponent,
         pathMatch: 'full'
     }
 ];
---
+++ reactive-UI/src/app/pages/break-down/break-down.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-break-down',
+  standalone: true,
+  imports: [],
+  templateUrl: './break-down.component.html',
+  styleUrl: './break-down.component.scss'
+})
+export class BreakDownComponent {
+
+}
