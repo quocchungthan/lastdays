d9758e5b8df43f7c0a7f5fc1d1a47700553f35fc
Chung Than Quoc <thanc.ext@medgate.ch>
2024-04-03 11:07:32 +0700

    router link active

---
+++ reactive-UI/src/ultilities/icons/google-swipe/google-swipe.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'icon-google-swipe',
+  standalone: true,
+  imports: [],
+  templateUrl: './google-swipe.component.html',
+  styleUrl: './google-swipe.component.scss'
+})
+export class GoogleSwipeComponent {
+
+}
--- reactive-UI/src/ultilities/layout/sidebar/sidebar.component.ts
+++ reactive-UI/src/ultilities/layout/sidebar/sidebar.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { GoogleSwipeComponent } from '../../icons/google-swipe/google-swipe.component';
 
 @Component({
   selector: 'sidebar',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, GoogleSwipeComponent],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
