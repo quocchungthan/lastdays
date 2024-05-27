4983db2b86eba46a482651e6485e89379e6e8628
Than Quoc Chung <quocchung.than@gmail.com>
2024-05-28 03:18:34 +0700

    clunky dropdown as a tool to select drawing tool

---
+++ reactive-UI/src/ultilities/controls/ui-dropdown/ui-dropdown.component.ts
@@ -0,0 +1,33 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+export interface DropDownItem {
+  id: string;
+  name: string;
+}
+
+@Component({
+  selector: 'ui-dropdown',
+  standalone: true,
+  imports: [],
+  templateUrl: './ui-dropdown.component.html',
+  styleUrl: './ui-dropdown.component.scss'
+})
+export class UiDropdownComponent {
+  @Input()
+  selectedId: string = '';
+  @Input()
+  label: string = '';
+  @Input()
+  items: DropDownItem[] = [];
+  @Output()
+  itemSelected = new EventEmitter<string>();
+
+  select(event: MouseEvent, selectedId: string) {
+    event.preventDefault();
+    this.itemSelected.emit(selectedId);
+  }
+
+  get selectedName() {
+    return this.items.find(x => x.id === this.selectedId)?.name;
+  }
+}
