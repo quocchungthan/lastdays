6894edd7f671e4fa43aaed7435b4c7f16774d72d
Than Quoc Chung <quocchung.than@gmail.com>
2024-05-27 13:41:37 +0700

    topbar

---
+++ reactive-UI/src/ultilities/chat/chatbox/chatbox.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chatbox',
+  standalone: true,
+  imports: [],
+  templateUrl: './chatbox.component.html',
+  styleUrl: './chatbox.component.scss'
+})
+export class ChatboxComponent {
+
+}
