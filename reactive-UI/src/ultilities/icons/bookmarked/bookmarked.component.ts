5c7488fd2cf7a4f06fab572838687c4815bfecd8
Than Quoc Chung <quocchung.than@gmail.com>
2024-05-28 02:21:07 +0700

    bookmark, icon button -> done fundamental, can start with konva feature/ commands

---
+++ reactive-UI/src/ultilities/icons/bookmarked/bookmarked.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'icon-bookmarked',
+  standalone: true,
+  imports: [],
+  templateUrl: './bookmarked.component.html',
+  styleUrl: './bookmarked.component.scss'
+})
+export class BookmarkedComponent {
+
+}
