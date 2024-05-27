aa6377ec27de6ee3beb805a8925b1cc85ed3f648
Than Quoc Chung <quocchung.than@gmail.com>
2024-05-27 13:04:00 +0700

    structure

---
+++ reactive-UI/src/app/pages/board-creation/board-creation.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-board-creation',
+  standalone: true,
+  imports: [],
+  templateUrl: './board-creation.component.html',
+  styleUrl: './board-creation.component.scss'
+})
+export class BoardCreationComponent {
+
+}
---
+++ reactive-UI/src/app/pages/board-detail/board-detail.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-board-detail',
+  standalone: true,
+  imports: [],
+  templateUrl: './board-detail.component.html',
+  styleUrl: './board-detail.component.scss'
+})
+export class BoardDetailComponent {
+
+}
