432f3c8b589270728cc0aeba088e869c1dec1b98
Than Quoc Chung <quocchung.than@gmail.com>
2024-05-30 19:39:42 +0700

    more tool

--- reactive-UI/src/app/pages/board-detail/Cursor.manager.ts
+++ reactive-UI/src/app/pages/board-detail/Cursor.manager.ts
@@ -1,14 +1,23 @@
 export interface ICursorManager {
     grabbing(): void;
     reset(): void;
+    pencil(): void;
 }
 
-export class CursorManager {
+export class CursorManager implements ICursorManager {
+    pencil(): void {
+        throw new Error('Method not implemented.');
+    }
+    
     grabbing() {
-        document.body.style.cursor = 'grab';
+        this._getCssBearer().style.cursor = 'grab';
     }
 
     reset() {
-        document.body.style.cursor = 'default';
+        this._getCssBearer().style.cursor = 'default';
+    }
+
+    private _getCssBearer() {
+        return document.body;
     }
 }
