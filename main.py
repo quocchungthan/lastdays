6af7643efa02e8e261ed406ce924700e73b2d8ea
Than Quoc Chung <chung.thanquoc@gmail.com>
2024-06-06 16:57:34 +0700

    first endpoint with openai

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ from business.services.loggerInteface import ILogger
 from configurations.env import Env
 from plugs.consoleloggers.consolestream import ConsoleLogger
 from plugs.notion.queryOnTableService import QueryOnTableService
+from plugs.openai.chatCompeleteService import ChatCompeleteService
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     logger = ConsoleLogger()
     envVars = Env()
     queryOnTableService = QueryOnTableService(logger, envVars.NOTION_TOKEN_V2, envVars.NOTION_WORKSPACE_ID, envVars.NOTION_TABLE_ID)
-
     queryOnTableService.configCheck()
     
     yield
@@ -21,3 +21,11 @@ app = FastAPI(lifespan=lifespan)
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/hello-ai")
+async def rootAI():
+    logger = ConsoleLogger()
+    envVars = Env()
+    client = ChatCompeleteService(logger, envVars.OPENAI_API_KEY)
+    client.helloWord()
+    return {"message": "AI said Hello World"}
