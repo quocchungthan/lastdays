72cae49243d33b248376bdbc89220d3c1c9802a0
Than Quoc Chung <chung.thanquoc@gmail.com>
2024-06-10 10:18:06 +0700

    he is doing DI

---
+++ business/services/readDrawingServiceInterface.py
@@ -0,0 +1,6 @@
+from typing import Any
+
+class IReadDrawingService:
+    def describeWhatUserDrawn(self, drawingJson: str, contextJson: str = None):
+        """Load in the file for extracting text."""
+        pass
