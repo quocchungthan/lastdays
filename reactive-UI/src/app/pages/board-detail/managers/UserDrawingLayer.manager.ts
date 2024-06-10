67157ef590bba4ce3f82750baea3c7d551482a7f
Than Quoc Chung <chung.thanquoc@gmail.com>
2024-06-10 16:22:57 +0700

    put sticky note down

--- reactive-UI/src/app/pages/board-detail/managers/UserDrawingLayer.manager.ts
+++ reactive-UI/src/app/pages/board-detail/managers/UserDrawingLayer.manager.ts
@@ -8,6 +8,10 @@ import { Injectable, OnDestroy } from "@angular/core";
 import { KonvaObjectService } from "../../../services/3rds/konva-object.service";
 import { DrawingObjectService } from "../../../services/data-storages/drawing-object.service";
 import { DrawingObject } from "../../../services/data-storages/entities/DrawingObject";
+import { StickyNoteCommands } from "../commands/sticky-notes.command";
+import { CursorManager } from "./Cursor.manager";
+import { isNil } from "lodash";
+import { Subject } from "rxjs";
 
 @Injectable()
 export class UserDrawingLayerManager implements OnDestroy {
@@ -18,23 +22,30 @@ export class UserDrawingLayerManager implements OnDestroy {
     }
     private _tool: string = '';
     private _pencil!: PencilCommands;
+    private _stickyNote: StickyNoteCommands;
     private _viewPort!: Konva.Stage;
     private _boardId: string = '';
+    private _placeholderLayer: Konva.Layer;
+    private _drawingToolEnd = new Subject<void>();
 
     constructor(
         _konvaObjects: KonvaObjectService,
         private _events: ViewPortEventsManager, 
         private boards: BoardsService,
+        private _cursor: CursorManager,
         private _urlExtractor: UrlExtractorService,
         private _drawingObjects: DrawingObjectService) {
         this._drawingLayer = new Konva.Layer();
+        this._placeholderLayer = new Konva.Layer();
         this._pencil = new PencilCommands(this._drawingLayer);
+        this._stickyNote = new StickyNoteCommands(this._placeholderLayer, this._drawingLayer);
         _konvaObjects.viewPortChanges.subscribe(s => {
             if (s.children.some(x => x === this._drawingLayer)) {
                 return;
             }
             this._viewPort = s;
             this._viewPort.add(this._drawingLayer);
+            this._viewPort.add(this._placeholderLayer);
         });
         
         this._startSubscribingEvents();
@@ -43,6 +54,14 @@ export class UserDrawingLayerManager implements OnDestroy {
                 this._boardId = id;
                 this._loadExistingDrawings();
             });
+    }
+
+    get tool() {
+        return this._tool;
+    }
+
+    onDrawingToolEnd() {
+        return this._drawingToolEnd.asObservable();
     }
 
     ngOnDestroy() {
@@ -106,18 +125,52 @@ export class UserDrawingLayerManager implements OnDestroy {
         if (this._tool === PencilCommands.CommandName) {
             // TODO: Flow
             // TODO: Command via chat
-            var brandNewDrawing = this._pencil.penUp();
-            const newDrawingObject = new DrawingObject();
-            newDrawingObject.boardId = this._boardId;
-            newDrawingObject.konvaObject = brandNewDrawing;
-            this._drawingObjects.create(newDrawingObject)
-                .then((x) =>{
-                    console.log('Saved', x);
-                });
+            const brandNewDrawing = this._pencil.penUp();
+            this._syncToDb(brandNewDrawing);
         }
+
+        if (this._tool === StickyNoteCommands.CommandName) {
+            const brandNewDrawing = this._stickyNote.putnew();
+            this._drawingToolEnd.next();
+            this._syncToDb(brandNewDrawing);
+        }
+    }
+
+    private _syncToDb(brandNewDrawing?: Konva.Shape) {
+        if (isNil(brandNewDrawing)) {
+            return;
+        }
+        const newDrawingObject = new DrawingObject();
+        newDrawingObject.boardId = this._boardId;
+        newDrawingObject.konvaObject = brandNewDrawing;
+        this._drawingObjects.create(newDrawingObject)
+            .then((x) => {
+                console.log('Saved', x);
+            });
     }
 
     setTool(tool: string) {
         this._tool = tool;
+        this._setupSpecialCursor();
+    }
+
+    private _setupSpecialCursor() {
+        if (this._tool !== StickyNoteCommands.CommandName) {
+            this._stickyNote.detachPlaceholder();
+            return;
+        }
+
+        this._stickyNote.attachStickyNotePlaceholder()
+            .then((placeholder) => {
+                this._cursor.grabbing();
+                this._events.onCursorMove()
+                    .subscribe((p) => {
+                        if (!placeholder.visible()) {
+                            return;
+                        }
+
+                        placeholder.position({x: p.x - placeholder.width() / 2, y: p.y - placeholder.height() / 2});
+                    })
+            })
     }
 }
