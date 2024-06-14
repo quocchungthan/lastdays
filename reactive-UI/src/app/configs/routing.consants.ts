f9a07226ee1e51d091acf12965d54bdb4e7d083e
Than Quoc Chung <chung.thanquoc@gmail.com>
2024-06-14 17:02:34 +0700

    wrong build tool so server.ts does not work

--- reactive-UI/src/app/configs/routing.consants.ts
+++ reactive-UI/src/app/configs/routing.consants.ts
@@ -1,4 +1,5 @@
 export const SEGMENT_TO_BOARD_DETAIL = 'board';
 export const SEGMENT_TO_IDENTITY_PAGE = 'identity';
 export const WEB_SOCKET_PORT = 8080;
-export const WEB_SOCKET_SERVER = 'ws://localhost:' + WEB_SOCKET_PORT;
+export const WEB_SOCKET_PATH = '/socket';
+export const WEB_SOCKET_SERVER = 'ws://localhost:' + WEB_SOCKET_PORT + WEB_SOCKET_PATH;
