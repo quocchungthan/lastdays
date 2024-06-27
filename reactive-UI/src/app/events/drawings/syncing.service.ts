b8924ed148c6959f0af7aa379c5588993c8011b7
Than Quoc Chung <chung.thanquoc@gmail.com>
2024-06-27 15:13:39 +0700

    sync the drawings

--- reactive-UI/src/app/events/drawings/syncing.service.ts
+++ reactive-UI/src/app/events/drawings/syncing.service.ts
@@ -2,13 +2,14 @@ import { Injectable } from '@angular/core';
 import { BaseEvent } from './EventQueue';
 import { WebSocketSubject, webSocket } from 'rxjs/webSocket'
 import { WEB_SOCKET_SERVER } from '../../configs/routing.consants';
-import { EMPTY, catchError, tap } from 'rxjs';
+import { BehaviorSubject, EMPTY, catchError, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SyncingService {
   private _ws?: WebSocketSubject<BaseEvent>;
+  private _allEvents = new BehaviorSubject<BaseEvent[]>([]);
 
   constructor() {
   }
@@ -20,16 +21,22 @@ export class SyncingService {
     return this;
   }
 
+  public peerCheck(currentEvents: BaseEvent[]) {
+    this._allEvents.next(currentEvents);
+
+    return this;
+  }
+
   public trySendEvent(event: BaseEvent) {
     this._ws?.next(event);
   }
 
-  public onEventAdded(boardId: string) {
-    console.log('got subscription');
+  public onEventAdded() {
+    return this._allEvents.asObservable();
   }
 
-  private _onMessageReceive(data: any) {
-    console.log('received', data);
+  private _onMessageReceive(data: BaseEvent) {
+    this._allEvents.next([...this._allEvents.value, data])
   }
 
   private _listen() {
