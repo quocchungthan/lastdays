8b4b7632a9d974171dc463d849ee2dce894ecca5
Than Quoc Chung <chung.thanquoc@gmail.com>
2024-06-28 14:13:43 +0700

    multiple lines

--- scripts/runapp.bat
+++ scripts/runapp.bat
@@ -28,7 +28,11 @@ cd /d %USERPROFILE%\Documents\lastdays\reactive-UI
 start "Angular app" ng serve --port 4201 -o
 
 REM Activate your Conda environment and start uvicorn in another window
-start "Python App" cd /d %USERPROFILE%\Documents\lastdays && conda activate deploymentenv && conda install python=3.12.2 --quiet && pip install -r requirements.txt --quiet && uvicorn main:app --reload
+cd /d %USERPROFILE%\Documents\lastdays
+conda activate deploymentenv 
+conda install python=3.12.2 --quiet 
+pip install -r requirements.txt --quiet 
+uvicorn main:app --reload
 
 REM Pause to keep the command windows open (optional)
 pause
