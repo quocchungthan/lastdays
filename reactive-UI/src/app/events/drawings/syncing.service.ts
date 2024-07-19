6648f0bae1461f79f135589fac487dccc4e51afc
Than Quoc Chung <chung.thanquoc@gmail.com>
2024-07-19 11:35:42 +0700

    separate on add and on reset

--- reactive-UI/src/app/events/drawings/syncing.service.ts
+++ reactive-UI/src/app/events/drawings/syncing.service.ts
@@ -1,16 +1,19 @@
 import { Injectable } from '@angular/core';
-import { BaseEvent, ParseToBaseEvent, ToDrawingEvent } from './EventQueue';
+import { BaseEvent, ParseToBaseEvent, ToBaseEvent, ToDrawingEvent } from './EventQueue';
 import { WebSocketSubject, webSocket } from 'rxjs/webSocket'
 import { WEB_SOCKET_SERVER } from '../../configs/routing.consants';
-import { BehaviorSubject, EMPTY, catchError, tap } from 'rxjs';
+import { BehaviorSubject, EMPTY, Subject, catchError, filter, map, tap } from 'rxjs';
 import { WSEvent, WSEventType } from '../to-python-server/web-socket-model';
 import { ComparisonResult, EventsCompositionService } from './events-composition.service';
 import { ConflictResolverService } from './conflict-resolver.service';
+import { isNil } from 'lodash';
 
 @Injectable()
 export class SyncingService {
   private _ws?: WebSocketSubject<WSEvent>;
-  private _allEvents = new BehaviorSubject<BaseEvent[]>([]);
+  private _allEventsChanges = new BehaviorSubject<BaseEvent>(null!);
+  private _allEventsReset = new Subject<void>();
+  private _allEventsBaseEvent: BaseEvent[] = [];
 
   constructor(private _eventsCompositionService: EventsCompositionService, private _conflictResolver: ConflictResolverService) {
   }
@@ -23,7 +26,8 @@ export class SyncingService {
   }
 
   public peerCheck(currentEvents: BaseEvent[]) {
-    this._allEvents.next(currentEvents);
+    this._allEventsBaseEvent = currentEvents;
+    this._allEventsReset.next();
     this._askOtherClients();
     return this;
   }
@@ -36,7 +40,7 @@ export class SyncingService {
   }
 
   public trySendEvent(event: BaseEvent) {
-    this._allEvents.next([...this._allEvents.value, event]);
+    this._allEventsBaseEvent.push(event);
     this._ws?.next({
       data: event,
       type: WSEventType.DRAWING_EVENT
@@ -44,19 +48,24 @@ export class SyncingService {
   }
 
   public onEventAdded() {
-    return this._allEvents.asObservable();
+    return this._allEventsChanges.pipe((filter(x => !isNil(x))));
+  }
+
+  public onEventsReset() {
+    return this._allEventsReset.pipe((map(() => this._allEventsBaseEvent)));
   }
 
   private _onMessageReceive(data: WSEvent) {
     console.log('Receiving ', data);
     switch (data.type) {
       case WSEventType.DRAWING_EVENT:
-        this._allEvents.next([...this._allEvents.value, data.data as BaseEvent]);
+        this._handleAdded(data.data);
         break;
       case WSEventType.ASK_OTHER_CLIENTS:
+        console.log('responding that', this._allEventsBaseEvent);
         this._ws?.next({
           type: WSEventType.OTHER_CLIENT_RESPONDED,
-          data: this._allEvents.value
+          data:  this._allEventsBaseEvent
         });
         break;
       case WSEventType.OTHER_CLIENT_RESPONDED:
@@ -70,6 +79,18 @@ export class SyncingService {
     }
   }
 
+  private _handleAdded(data: BaseEvent | string | undefined | BaseEvent[]) {
+    const received = ParseToBaseEvent(data)!;
+    this._allEventsBaseEvent.push(received);
+    this._allEventsChanges.next(received);
+  }
+
+  private _handleAddedMultiple(data: WSEvent) {
+    (data.data as any[]).forEach(element => {
+      this._handleAdded(element);
+    });
+  }
+
   private _handleComparisionREsult(comparison: ComparisonResult, data: WSEvent) {
     console.debug(comparison.toString());
     switch (comparison) {
@@ -78,14 +99,16 @@ export class SyncingService {
         return;
       // TODO: If the up coming is more than all -> do render, do save
       case ComparisonResult.ADDED:
-        this._allEvents.next([
-          ...this._allEvents.value,
-          ...((data.data as any[]).map(x => ParseToBaseEvent(x)!)).slice(this._allEvents.value.length)
-        ]);
+        this._handleAddedMultiple(data);
         return;
       // TODO: If they're conflict at some point -> replace the local storage, build all again with the up coming
       case ComparisonResult.CONFLICT:
-        this._allEvents.next(this._conflictResolver.resolve((data.data as any[]).map(x => ParseToBaseEvent(x)!) as BaseEvent[], this._allEvents.value));
+        const solvedConflict = this._conflictResolver.resolve((data.data as any[]).map(x => ParseToBaseEvent(x)!) as BaseEvent[],
+          this._allEventsBaseEvent);
+        if (solvedConflict !== this._allEventsBaseEvent) {
+          this._allEventsBaseEvent = solvedConflict;
+          this._allEventsReset.next();
+        }
         return;
       default:
         throw Error("Not handled", comparison);
