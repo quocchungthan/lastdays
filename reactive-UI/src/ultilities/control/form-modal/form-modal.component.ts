344d908e6d745bfc211753ef463bc844c9c48029
Than Quoc Chung <chung.thanquoc@gmail.com>
2024-07-22 13:45:59 +0700

    text on dialog translatable

--- reactive-UI/src/ultilities/control/form-modal/form-modal.component.ts
+++ reactive-UI/src/ultilities/control/form-modal/form-modal.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormModalService } from '../form-modal.service';
 import { filter } from 'rxjs';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'form-modal',
@@ -12,8 +13,15 @@ import { filter } from 'rxjs';
 })
 export class FormModalComponent implements OnInit {
   isOpening = false;
+  cancelText = 'FORM_DIALOG_CANCEL';
+  submitText = 'FORM_DIALOG_SUBMIT';
 
-  constructor(private _formModalService: FormModalService) {
+  constructor(private _formModalService: FormModalService, private _translateService: TranslateService) {
+    this._translateService.get([this.cancelText, this.submitText])
+      .subscribe((result) => {
+        this.cancelText = result[this.cancelText];
+        this.submitText = result[this.submitText];
+      });
   }
 
   ngOnInit(): void {
