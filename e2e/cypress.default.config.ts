8542700090697fff74cb214b605c4a0c5bd9524a
Than Quoc Chung <chung.thanquoc@gmail.com>
2024-08-13 14:41:12 +0700

    test different env

---
+++ e2e/cypress.default.config.ts
@@ -0,0 +1,11 @@
+import {addMatchImageSnapshotPlugin} from '@simonsmith/cypress-image-snapshot/plugin'
+
+export const defaultConfiguration: Cypress.ConfigOptions<any> = {
+   e2e: {
+     setupNodeEvents(on, config) {
+       // implement node event listeners here
+       addMatchImageSnapshotPlugin(on);
+     },
+     baseUrl: 'http://localhost'
+   },
+ };
--- e2e/cypress.local.config.ts
+++ e2e/cypress.local.config.ts
@@ -1,4 +1,6 @@
 import { defineConfig } from "cypress";
 import { defaultConfiguration } from "./cypress.default.config";
 
+defaultConfiguration.e2e!.baseUrl = 'http://localhost:4200';
+
 export default defineConfig(defaultConfiguration);
