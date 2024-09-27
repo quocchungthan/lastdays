61e2bf0e5a6169fa4d05841db071693500d63668
Than Quoc Chung <chung.thanquoc@gmail.com>
2024-09-27 15:48:37 +0700

    draft header and footer

--- reactive-UI/src/app-ecom/components/navigation/navigation.component.ts
+++ reactive-UI/src/app-ecom/components/navigation/navigation.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import $ from 'jquery';
 
 @Component({
   selector: 'app-navigation',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.scss'
 })
-export class NavigationComponent {
+export class NavigationComponent implements AfterViewInit {
+  ngAfterViewInit(): void {
+    $('.header').on('click', '.search-toggle', function(e) {
+      var selector = $(this).data('selector');
+    
+      $(selector).toggleClass('show').find('.search-input').focus();
+      $(this).toggleClass('active');
+    
+      e.preventDefault();
+    });
+  }
+  searchTerm: string = '';
 
+  onSearch() {
+    console.log('Searching for:', this.searchTerm);
+    // You can add functionality to handle the search
+  }
 }
