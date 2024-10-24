00856178447b9547b878204b12f49ffd10506c59
Than Quoc Chung <chung.thanquoc@gmail.com>
2024-10-24 08:34:27 +0700

    setup host for migration generation

--- scheduledjobs/Program.cs
+++ scheduledjobs/Program.cs
@@ -1,4 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using DataLayer.EFCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using System;
 
-Console.WriteLine("Hello, World!");
+var host = Host.CreateDefaultBuilder(args)
+		.ConfigureAppConfiguration((context, config) =>
+		{
+			config.SetBasePath(AppContext.BaseDirectory)
+					.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+		})
+	 .ConfigureServices((context, services) =>
+	 {
+		 // Configure DbContext with SQL Server
+		 services.AddDbContext<AppDbContext>(options =>
+			  options.UseNpgsql("YourConnectionStringHere"));
+
+		 // Register other services as needed
+	 })
+	 .Build();
