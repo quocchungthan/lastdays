68a984b6f0addf74ae2f7959e915aaa96ff288b1
Than Quoc Chung <chung.thanquoc@gmail.com>
2024-11-20 15:30:52 +0700

    put the event back

--- lean/application.whiteboard/src/app/board-detail/board-detail.component.ts
+++ lean/application.whiteboard/src/app/board-detail/board-detail.component.ts
@@ -1,17 +1,29 @@
-import { AfterViewInit, Component, HostListener, OnDestroy } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  HostListener,
+  OnDestroy,
+} from '@angular/core';
 import { KONVA_CONTAINER } from '../../shared-configuration/html-ids.constants';
 import { KonvaObjectService } from '../services/konva-object.service';
 import { takeUntil } from 'rxjs/internal/operators/takeUntil';
 import { BackgroundLayerManager } from '../services/BackgroundLayer.manager';
 import { Subject } from 'rxjs';
+import { ViewPortEventsManager } from '../services/ViewPortEvents.manager';
+import Konva from 'konva';
+import { Wheel } from '../../share-models/Wheel';
 
 @Component({
   selector: 'app-board-detail',
   standalone: true,
   imports: [],
-  providers: [KonvaObjectService, BackgroundLayerManager],
+  providers: [
+    KonvaObjectService,
+    BackgroundLayerManager,
+    ViewPortEventsManager,
+  ],
   templateUrl: './board-detail.component.html',
-  styleUrl: './board-detail.component.scss'
+  styleUrl: './board-detail.component.scss',
 })
 export class BoardDetailComponent implements AfterViewInit, OnDestroy {
   KONVA_CONTAINER = KONVA_CONTAINER;
@@ -21,8 +33,11 @@ export class BoardDetailComponent implements AfterViewInit, OnDestroy {
   /**
    *
    */
-  constructor(private _konvaObjectService: KonvaObjectService, private _backgroundLayerManager: BackgroundLayerManager) {
-  }
+  constructor(
+    private _konvaObjectService: KonvaObjectService,
+    private _backgroundLayerManager: BackgroundLayerManager,
+    private _interactiveEventManager: ViewPortEventsManager
+  ) {}
 
   @HostListener('window:resize')
   public onWindowResize(e: any) {
@@ -36,13 +51,76 @@ export class BoardDetailComponent implements AfterViewInit, OnDestroy {
   public ngOnDestroy(): void {
     this.unsubscribe$.next();
     this.unsubscribe$.complete();
+    this._interactiveEventManager.ngOnDestroy();
+    this._backgroundLayerManager.ngOnDestroy();
   }
 
   private _resetTheViewPort() {
     this._konvaObjectService.initKonvaObject();
     this._konvaObjectService.adaptViewPortSize();
-    this._konvaObjectService.viewPortChanges.pipe(takeUntil(this.unsubscribe$)).subscribe(() => {
-      this._backgroundLayerManager.drawBackground();
-    });
+    this._konvaObjectService.viewPortChanges
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe((stage) => {
+        this._backgroundLayerManager.drawBackground();
+        this.initiateBasedOnBackgroundStage(stage);
+      });
+  }
+
+  private initiateBasedOnBackgroundStage(stage: Konva.Stage) {
+    this._interactiveEventManager
+      .onDragStart()
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe(() => {
+        // this._cursorManager.grabbing();
+      });
+
+    this._interactiveEventManager
+      .onDragEnd()
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe(() => {
+        this._backgroundLayerManager.putTheRuler();
+      });
+
+    this._interactiveEventManager
+      .onTouchEnd()
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe(() => {
+        // if (!this.tool) {
+        //     this._cursorManager.reset();
+        // }
+      });
+
+    this._interactiveEventManager
+      .onWheel()
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe((wheelEventData) => {
+        this._backgroundLayerManager.putTheRuler();
+        this._onRequestZooming(stage, wheelEventData);
+      });
+  }
+
+  private _onRequestZooming(stage: Konva.Stage, wheelData: Wheel) {
+    const scaleBy = 1.01;
+    const oldScale = stage.scaleX();
+    const pointer = stage.getPointerPosition();
+
+    if (pointer === null) {
+      //   console.log('Pointer can\'t be null');
+      return;
+    }
+
+    const mousePointTo = {
+      x: (pointer.x - stage.x()) / oldScale,
+      y: (pointer.y - stage.y()) / oldScale,
+    };
+
+    const zoomInPercentage =
+      wheelData.direction > 0 ? oldScale * scaleBy : oldScale / scaleBy;
+    stage.scale({ x: zoomInPercentage, y: zoomInPercentage });
+    const newPos = {
+      x: pointer.x - mousePointTo.x * zoomInPercentage,
+      y: pointer.y - mousePointTo.y * zoomInPercentage,
+    };
+    stage.position(newPos);
   }
 }
---
+++ lean/application.whiteboard/src/app/services/ViewPortEvents.manager.ts
@@ -0,0 +1,152 @@
+import Konva from 'konva';
+import { Observable, Subject, debounceTime, filter, map, takeUntil } from 'rxjs';
+import { Injectable, OnDestroy } from '@angular/core';
+import { Point } from '../../share-models/Point';
+import { Wheel } from '../../share-models/Wheel';
+import { KonvaObjectService } from './konva-object.service';
+import isNil from 'lodash/isNil';
+
+export enum KonvaMouseButton {
+    LEFT = 0,
+    MIDDLE = 1,
+    RIGHT = 2,
+}
+
+@Injectable()
+export class ViewPortEventsManager implements OnDestroy {
+    private unsubscribe$ = new Subject<void>();
+    private _dragStart: Subject<Point | null>;
+    private _dragEnd: Subject<Point | null>;
+    private _wheel: Subject<Wheel | null>;
+    private _mouseEnter: Subject<void>;
+    private _cursorMove: Subject<Point | null>;
+    private _mouseOut: Subject<void>;
+    private _touchStart: Subject<Point | null>;
+    private _touchEnd: Subject<Point | null>;
+    private _touchMove: Subject<Point | null>;
+    private _viewPort!: Konva.Stage;
+    
+    constructor(_konvaObjects: KonvaObjectService) {
+        _konvaObjects.viewPortChanges.pipe(takeUntil(this.unsubscribe$)).subscribe(s => {
+            this._viewPort = s;
+            this._registerEventListener();
+        })
+        this._dragStart = new Subject<Point | null>();
+        this._dragEnd = new Subject<Point | null>();
+        this._touchStart = new Subject<Point | null>();
+        this._touchEnd = new Subject<Point | null>();
+        this._touchMove = new Subject<Point | null>();
+        this._cursorMove = new Subject<Point | null>();
+        this._mouseEnter = new Subject<void>();
+        this._mouseOut = new Subject<void>();
+        this._wheel = new Subject<Wheel | null>();
+    }
+    ngOnDestroy() {
+        this.unsubscribe$.next();
+        this.unsubscribe$.complete();
+    }
+
+    public onMouseEnter() {
+        return this._mouseEnter.asObservable();
+    }
+
+    public onMouseOut() {
+        return this._mouseOut.asObservable();
+    }
+
+    public onCursorMove() {
+        return this._notNilPointsAndRemoveDuplicatedEvents(this._cursorMove.asObservable());
+    }
+
+    public onWheel() {
+        return this._wheel.asObservable().pipe((filter(x => !isNil(x) && !isNil(x.cursor))), map(x => x as Wheel), debounceTime(1));
+    }
+
+    private _registerEventListener() {
+        this._viewPort.on('dragstart', () => {
+            this._dragStart.next(this._currentRelativePosition());
+        });
+
+        this._viewPort.on('wheel', (e) => {
+            // how to scale? Zoom in? Or zoom out?
+            let direction: 1 | -1 = e.evt.deltaY > 0 ? 1 : -1;
+
+            // when we zoom on trackpad, e.evt.ctrlKey is true
+            // in that case lets revert direction
+            if (e.evt.ctrlKey) {
+                direction = direction > 0 ? -1 : 1;
+            }
+
+            this._wheel.next({
+                cursor: this._currentRelativePosition(),
+                direction
+            });
+        });
+
+        this._viewPort.on('dragend', () => {
+            this._dragEnd.next(this._currentRelativePosition());
+        });
+
+        this._viewPort.on('mousemove', () => {
+            this._cursorMove.next(this._currentRelativePosition());
+        });
+
+        this._viewPort.on('mouseenter', () => {
+            this._mouseEnter.next();
+        });
+
+        this._viewPort.on('mouseleave', () => {
+            this._mouseOut.next();
+        });
+
+        this._viewPort.on('mousedown touchstart', (e) => {
+            // TODO: right test to cover right click cuz we use it later for menu context
+            if (e.evt?.button === KonvaMouseButton.RIGHT) {
+                return;
+            }
+            this._touchStart.next(this._currentRelativePosition());
+        });
+          
+        this._viewPort.on('mousemove touchmove', (e) => {
+            if (e.evt?.button === KonvaMouseButton.RIGHT) {
+                return;
+            }
+            this._touchMove.next(this._currentRelativePosition());
+        });
+        
+        this._viewPort.on('mouseup touchend', (e) => {
+            if (e.evt?.button === KonvaMouseButton.RIGHT) {
+                return;
+            }
+            this._touchEnd.next(this._currentRelativePosition());
+        });
+    }
+
+    private _currentRelativePosition(): Point | null {
+        return this._viewPort.getRelativePointerPosition();
+    }
+
+    private _notNilPointsAndRemoveDuplicatedEvents(observer: Observable<Point | null>) {
+        return observer.pipe((filter(x => !isNil(x))), map(x => x as Point), debounceTime(1))
+    }
+
+    public onDragStart() {
+        return this._notNilPointsAndRemoveDuplicatedEvents(this._dragStart.asObservable());
+    }
+
+    public onDragEnd() {
+        return this._notNilPointsAndRemoveDuplicatedEvents(this._dragEnd.asObservable());
+    }
+
+    public onTouchStart() {
+        return this._notNilPointsAndRemoveDuplicatedEvents(this._touchStart.asObservable());
+    }
+
+    public onTouchMove() {
+        return this._notNilPointsAndRemoveDuplicatedEvents(this._touchMove.asObservable());
+    }
+
+    public onTouchEnd() {
+        return this._notNilPointsAndRemoveDuplicatedEvents(this._touchEnd.asObservable());
+    }
+}
