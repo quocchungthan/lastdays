2a9667b24feda760f4f7987954fc79f8151c887f
Than Quoc Chung <chung.thanquoc@gmail.com>
2024-11-20 14:14:51 +0700

    init new project without ssr

--- lean/board-only/src/app/common.definitions/id.constants.ts
+++
@@ -1,1 +0,0 @@
-export const KONVA_CONTAINER = 'KONVA_CONTAINER';
--- lean/board-only/src/app/common.definitions/local-entities/ALEvent.ts
+++
@@ -1,6 +0,0 @@
-import { BaseEntity } from "./BaseEntity";
-
-export interface ALEvent extends BaseEntity {
-   code: 'BoardCreated';
-   boardId: string;
-}
