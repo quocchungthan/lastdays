cf9969c9965ab8642972ec68642c50e2b7124dec
Than Quoc Chung <chung.thanquoc@gmail.com>
2024-11-21 09:57:18 +0700

    waiting screen

--- lean/application.whiteboard/src/app/board-auto-creation/board-auto-creation.component.ts
+++ lean/application.whiteboard/src/app/board-auto-creation/board-auto-creation.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 
 @Component({
   selector: 'app-board-auto-creation',
@@ -7,6 +7,11 @@ import { Component } from '@angular/core';
   templateUrl: './board-auto-creation.component.html',
   styleUrl: './board-auto-creation.component.scss'
 })
-export class BoardAutoCreationComponent {
-
+export class BoardAutoCreationComponent implements AfterViewInit {
+  ngAfterViewInit(): void {
+    // Simulate a loading delay (like waiting for an API or data to load)
+    setTimeout(() => {
+      // Here we can communicate with parent component to hide the loading screen
+    }, 5000); // Adjust time as needed
+  }
 }
