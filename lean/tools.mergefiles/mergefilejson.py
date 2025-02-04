04522c9ba742021a4128f107d99367ded1f62b2a
Than Quoc Chung <chung.thanquoc@gmail.com>
2025-02-04 10:52:30 +0700

    merge and sort

--- lean/tools.mergefiles/mergefilejson.py
+++ lean/tools.mergefiles/mergefilejson.py
@@ -23,10 +23,11 @@ def merge_json_files(old_file_path, new_file_path, output_file_path):
     with open(output_file_path, 'w', encoding='utf-8') as output_file:
         json.dump(sorted_merged_data, output_file, ensure_ascii=False, indent=4)
 
+
 # Define file paths relative to the base path
-old_file = os.path.join(BASE_PATH, 'Documents/resetthings6/RBCC/source/RBCC_Base/src/assets/api/langs/zh.json')
+old_file = os.path.join(BASE_PATH, 'Documents/resetthings6/RBCC/UI/src/assets/i18n/zh.json')
 new_file = os.path.join(BASE_PATH, 'Downloads/Translated_IQ4-CirrusPro_strings/zh.json')
-output_file = os.path.join(BASE_PATH, 'Documents/resetthings6/RBCC/source/RBCC_Base/src/assets/api/langs/zh.merged.json')
+output_file = os.path.join(BASE_PATH, 'Documents/resetthings6/RBCC/UI/src/assets/i18n/zh.merged.json')
 
 # Call the function to merge the files
 merge_json_files(old_file, new_file, output_file)
